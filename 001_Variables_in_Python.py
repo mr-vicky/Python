05f1b22ce07447cd4ea6828c20df6a5884e743d9
Vivek Sarade <viveksarade4949@gmail.com>
2023-08-27 22:53:39 +0530

    first Commit

---
+++ 001_Variables_in_Python.py
@@ -0,0 +1,20 @@
+# Variables
+a = 1925
+Name, Income, CompanyName = "Mr. Vicky", "1000000000", "Sarade Group"
+x = y = z = 1925
+
+print(Name)
+print(a)
+print(CompanyName)
+
+print(x) 
+print(y)
+print(z)
+
+# Python is Dynamically Typed Language
+x = 12
+print(type(x))
+x = 19.25
+print(type(x))
+x = "String"
+print(type(x))
