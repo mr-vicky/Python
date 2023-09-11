728eba123aa4628dc4bd32d92f4f8810d56b3fd0
Vivek Sarade <viveksarade4949@gmail.com>
2023-09-11 16:28:59 +0530

    Python

---
+++ 003_Bitwise_operators.py
@@ -0,0 +1,11 @@
+a = 2
+print(format(a, 'b')) # format() function
+
+# & Operation
+a = 10
+b = 1
+print("a & b :", a & b) # set's bit 1 if both bits are 1 otherwise ste's to 0
+print("a | b :", a | b) # set's bit to 1 if one of the bit is 1 otherwise set's to 0
+print("a ^ b :", a ^ b) # set's bit to 1 if both bits are different otherwise set's to 0
+print("a << b :", a << b) # Left Shift Operator: (2**b)*a
+print("a >> b :", a >> b) # Right shift Operator: a/(2**b)
