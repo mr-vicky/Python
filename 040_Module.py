5b01c11727f3174c27d3994a991389c2ec2d01e2
Vivek Sarade <viveksarade4949@gmail.com>
2023-11-18 15:59:37 +0530

    Python

---
+++ 040_Module.py
@@ -0,0 +1,11 @@
+import ModuleOne as mo
+
+print(mo.total_amount)
+
+a = 10
+b = 20
+
+print(mo.add(a, b))
+print(mo.sub(a, b))
+print(mo.multi(a, b))
+print(mo.div(a, b))
---
+++ ModuleOne.py
@@ -0,0 +1,19 @@
+total_amount = 1000000
+
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def multi(a, b):
+    return a * b
+def div(a, b):
+    return a/b
+
+if __name__ == "__main__":
+    print("from ModuleONe")
+    print(add(2, 3))
+    print(sub(2, 3))
+    print(multi(2, 3))
+    print(div(2, 3))
+
+
